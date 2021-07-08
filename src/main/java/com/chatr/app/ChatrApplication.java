2ed25b1dec8c3c24641f8331ab1989ffbdf79b5c
calcium <stephen.dery@gmail.com>
2021-07-08 18:45:41 -0400

    init spring boot app

---
+++ src/main/java/com/chatr/app/ChatrApplication.java
@@ -0,0 +1,13 @@
+package com.chatr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chatr/app/ChatrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

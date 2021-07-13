092034f288b3066e4389fe4eae7c9ece60475508
sved-dry <stephen.dery@gmail.com>
2021-07-13 19:55:03 -0400

    small rest endpoint

--- src/main/java/com/chatr/app/ChatrApplication.java
+++ src/main/java/com/chatr/app/ChatrApplication.java
@@ -3,7 +3,7 @@ package com.chatr.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.chatr")
 public class ChatrApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/chatr/controller/AliveController.java
@@ -0,0 +1,18 @@
+package com.chatr.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AliveController {
+
+    public AliveController() {
+
+    }
+
+    @GetMapping(value = "/hello")
+    ResponseEntity<String> get() {
+        return ResponseEntity.ok("hi there");
+    }
+}

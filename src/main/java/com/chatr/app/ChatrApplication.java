db7318c8a5cbafaf38d06a8ecc189f8aab098b0f
calcium <stephen.dery@gmail.com>
2021-07-17 09:55:26 -0400

    disable auto security

--- src/main/java/com/chatr/app/ChatrApplication.java
+++ src/main/java/com/chatr/app/ChatrApplication.java
@@ -2,8 +2,9 @@ package com.chatr.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.chatr")
+@SpringBootApplication(scanBasePackages = "com.chatr", exclude = SecurityAutoConfiguration.class)
 public class ChatrApplication {
 
     public static void main(String[] args) {
